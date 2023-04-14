9d701bd30a2e0fc90d66e8a93b9cdc5f9b03ec94
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-04-15 02:00:09 +0900

    eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express, {Express, Request, Response} from "express"
-import mongoose, {ConnectOptions} from "mongoose";
+import express, { Express, Request, Response } from "express";
+import mongoose, { ConnectOptions } from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./routes";
@@ -10,14 +10,17 @@ const app: Express = express();
 const port = process.env.PORT;
 const dbURI = "mongodb://127.0.0.1:27017/test";
 
-const mongooseOptions:ConnectOptions = {
-}
+const mongooseOptions: ConnectOptions = {};
 
 app.use(express.json());
+
 app.use(cors());
 
-mongoose.connect(dbURI,mongooseOptions).then(()=>console.log("MongoDB connected...")).catch((err)=>console.log(err));
+mongoose
+  .connect(dbURI, mongooseOptions)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
-app.listen(port,()=>{
-    console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
